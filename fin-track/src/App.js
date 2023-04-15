d98dc9a9d21a91b393abfa75fae08a952dad25c0
Godsheritage  Adeoye <Adeoyegodsheritage@gmail.com>
2023-04-15 18:01:36 -0400

    setting up the routes

--- fin-track/src/App.js
+++ fin-track/src/App.js
@@ -1,12 +1,18 @@
 import Home from "./pages/home/Home";
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      {/* <Route path="/" element={<Home/>}/>
+      <Route path="/" element={<Home/>}/> */}
+      {/* <Route path="/" element={<FinancialLiteracy/>}/> */}
+    </Routes>
+     
   );
+
+  
 }
 
 export default App;
--- fin-track/src/index.js
+++ fin-track/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+<Router>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+</Router>
 );
 
 
