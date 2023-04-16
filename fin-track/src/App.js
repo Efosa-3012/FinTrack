82a98e1cfad2fc6e130504d41db19d694a2b9eed
Efosa-3012 <57777213+Efosa-3012@users.noreply.github.com>
2023-04-15 22:17:27 -0400

    tese

--- fin-track/src/App.js
+++ fin-track/src/App.js
@@ -1,4 +1,5 @@
 import Home from "./pages/home/Home";
+// import Education from "./pages/Education/Education";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
